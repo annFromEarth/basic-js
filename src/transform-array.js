e3c984488cf516ab78fd74b063d9359ae5ea7c60
annFromEarth <dv_anna@mail.ru>
2023-04-15 23:15:20 +0300

    transforn-array v1

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,80 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {throw new Error("'arr' parameter must be an instance of the Array!")};
+  
+  let actions = ["--discard-next", "--discard-prev","--double-next","--double-prev"];
+
+  let new_arr=arr.slice(0, arr.length);
+
+  for(let i=0; i<new_arr.length; i++) {
+    if (new_arr[i] === "--discard-next") {
+      new_arr[i]="--exclude";
+      if (new_arr[i+1]) {
+        new_arr[i+1]="--exclude";
+      }
+    }
+
+    if (new_arr[i] === "--discard-prev") {
+      new_arr[i]="--exclude";
+      if (new_arr[i-1]) {
+        new_arr[i-1]="--exclude";
+      }
+    }
+
+    if (new_arr[i] === "--double-next") {
+      new_arr[i]="--exclude";
+      if (new_arr[i+1]) {
+        new_arr[i]=new_arr[i+1];
+      }
+    }
+
+    if (new_arr[i] === "--double-prev") {
+      new_arr[i]="--exclude";
+      if (new_arr[i-1]) {
+        new_arr[i]=new_arr[i-1];
+      }
+    }
+  }
+
+  return new_arr.filter(x=>x!="--exclude");
+
 }
+
+  // function discard_next(action_index, array) { 
+  //   array[action_index]="--exclude";
+  //   if (array[action_index+1]) {
+  //     array[action_index+1]="--exclude";
+  //     return array;
+  //   }
+  // }
+
+  // function discard_prev(action_index, array) { 
+  //   array[action_index]="--exclude";
+  //   if (array[action_index-1]) {
+  //     array[action_index-1]="--exclude";
+  //     return array;
+  //   }
+  // }
+  
+  // function double_next(action_index, array) { 
+  //   array[action_index]="--exclude";
+  //   if (array[action_index+1]) {
+  //     array[action_index]=array[action_index+1];
+  //     return array;
+  //   }
+  // }
+
+  // function double_prev(action_index, array) { 
+  //   array[action_index]="--exclude";
+  //   if (array[action_index-1]) {
+  //     array[action_index]=array[action_index-1];
+  //     return array;
+  //   }
+  // }
+
+
 
 module.exports = {
   transform
