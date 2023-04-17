cb1ad1a2ca06282bdc44c97bc5c1d4adae8bfc31
annFromEarth <dv_anna@mail.ru>
2023-04-17 12:01:13 +0300

    dns-stats v1

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -24,8 +24,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 function getDNSStats(domains) {
 
-  domains.forEach(x=> x.split('.').reverse());
-  console.dir(domains)
+  let domains_destructured=domains.map(x=> x.split('.').reverse());
+  let domain_names = [];
+  let domain_stats={};
+
+  domains_destructured.forEach(x => {
+    let prev_domain="";
+    for (let i=0; i<x.length; i++) {
+
+      let current_domain=`.`+x[i];
+      domain_names.push(`${prev_domain}${current_domain}`);
+      prev_domain=`${prev_domain}${current_domain}`;
+    }
+  })
+
+  let domain_names_checked = domain_names.slice(0, domain_names.length);
+
+  for (let j=0; j<domain_names_checked.length; j++) {
+
+    let counter=1;
+
+    if (domain_names_checked[j] !== "--checked") {
+
+      for (let k=j+1; k<domain_names_checked.length; k++) {
+        if (domain_names_checked[k] === domain_names_checked[j]) {
+          counter++;
+          domain_names_checked[k]="--checked";
+        }
+      }
+
+      domain_stats[domain_names_checked[j]] = counter;
+    }
+  }
+
+  return domain_stats;
   
 }
 
